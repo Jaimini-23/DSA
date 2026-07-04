ff7b8614e0c2a657bbd21637da9ecceed523cd54
Jaimini-23 <mayankjaimini23@gmail.com>
2026-07-04 14:19:18 +0530

    lower and upper bound

--- Binary Search/BinarySearch.cpp
+++ Binary Search/BinarySearch.cpp
@@ -30,10 +30,44 @@ int targetIndex_sortedArray(vector<int> &arr, int n, int r) {
 }
 
 
+int lowerBound(vector<int> &arr ,int n, int r) {       // smallest index such that arr[index] >= r
+    // In C++ STL, lower_bound(arr.begin(), arr.end(), r) - arr.begin()
+    // TC: O(logn)_with_base_2
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        if(arr[mid] >= r) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
 
 
-
-
+int upperBound(vector<int> &arr ,int n, int r) {       // smallest index such that arr[index] > r
+    // In C++ STL, upper_bound(arr.begin(), arr.end(), r) - arr.begin()
+    // TC: O(logn)_with_base_2
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        if(arr[mid] > r) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
 
 
 
@@ -57,7 +91,9 @@ int main() {
     vector<int> arr(n);
     for(int i=0;i<n;i++) cin >> arr[i];
 
-    cout << targetIndex_sortedArray(arr,n,r);
+    // cout << targetIndex_sortedArray(arr,n,r);
+    // cout << lowerBound(arr,n,r);
+    // cout << upperBound(arr,n,r);
     return 0;
 }
 
