96516f5af794a1bdbefbd83bb6dd25d788211fd5
Jaimini-23 <mayankjaimini23@gmail.com>
2026-07-05 22:57:37 +0530

    Koko Eating Bananas

--- Binary Search/BinarySearch.cpp
+++ Binary Search/BinarySearch.cpp
@@ -433,6 +433,71 @@ int nthRoot(int n, int m) {
 }
 
 
+long long calculateTotalHours(vector<int>& piles, int speed) {
+    int n = piles.size();
+    long long totalHours = 0;
+    for (int i = 0; i < n; i++) {
+        totalHours += (piles[i] + speed - 1) / speed;
+    }
+    return totalHours;
+}
+int kokoEatingBananas(vector<int> piles,int n, int h) {
+    // n piles of bananas and needs to eat all of them within a deadline of hours
+    // minimum integer hourly eating rate
+    // Brute (TC: O(n × maxi) ans SC: O(1))
+    // Find maximum pile
+    // int maxi = INT_MIN;
+    // for(int i = 0; i < n; i++) {
+    //     maxi = max(maxi, piles[i]);
+    // }
+    // // Try every possible eating speed
+    // for (int k = 1; k <= maxi; k++) {
+    //     int totalHours = 0;
+    //     // Calculate total hours for current speed k
+    //     for (int i = 0; i < n; i++) {
+    //         totalHours += ceil((double)piles[i] / k);
+    //     }
+    //     // If Koko can finish within h hours
+    //     if (totalHours <= h)
+    //         return k;
+    // }
+    // return maxi;
+
+    // Optimize
+    // TC: O(n × log(maxElement)) and SC: O(1)
+    int low = 1;
+    int high = *max_element(piles.begin(), piles.end());
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        long long totalHours = calculateTotalHours(piles, mid);
+        if (totalHours <= h) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -459,7 +524,11 @@ int main() {
     // cout << peakElement(arr,n);
     
     // cout << squareRoot(n);
-    cout << nthRoot(n,r);
+    // cout << nthRoot(n,r);
+    cout << kokoEatingBananas(arr,n,r);
+
+
+
 
     return 0;
 }
