986ad900b0ca38967941da9112a21f338b342c18
Jaimini-23 <mayankjaimini23@gmail.com>
2026-08-01 12:04:22 +0530

    Kth Missing number

--- Binary Search/BinarySearch_2.cpp
+++ Binary Search/BinarySearch_2.cpp
@@ -16,6 +16,8 @@ int findDays(vector<int> &weight, int cap) {
     return days;
 }
 int leastWeightCapacity(vector<int> &weight, int n, int days) {
+    // TC: O(n × log(sum(weights)−max(weights))) and SC: O(1)
+
     int low = *max_element(weight.begin(),weight.end());
     int high = accumulate(weight.begin(),weight.end(),0LL);
 
@@ -26,13 +28,54 @@ int leastWeightCapacity(vector<int> &weight, int n, int days) {
         else low = mid + 1;
     }
     return low;
-
-    // TC: O(n × log(sum(weights)−max(weights))) and SC: O(1)
 }
+
+
+int kthMissingNumber(vector<int> arr, int n, int k) {
+    // Brute (TC: O(n) and SC: O(1)) 
+    // for(int i=0; i<n; i++) {
+    //     if(arr[i] <= k) k++;
+    //     else break;
+    // }
+    // return k;
+
+    // optimal (TC: O(logn) and SC: O(1))
+    int low = 0;
+    int high = n - 1;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        int missing = arr[mid] - (mid + 1);
+        if(missing < k) low = mid + 1;
+        else high = mid - 1;
+    }
+    return k + high + 1;          // or return low + k;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 int main() {
+    int n ,r;
+    cin >> n >> r;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin >> arr[i];
 
+    // cout << leastWeightCapacity(arr,n,r);
+    cout << kthMissingNumber(arr,n,r);
     return 0;
 }
+
+
+
+
