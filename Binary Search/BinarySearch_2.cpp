d0defd1dd4d69ae13310e821d264e2b6fee4a176
Jaimini-23 <mayankjaimini23@gmail.com>
2026-08-20 19:58:03 +0530

    Median of two Sorted arrays

--- Binary Search/BinarySearch_2.cpp
+++ Binary Search/BinarySearch_2.cpp
@@ -139,7 +139,33 @@ int splitArray(vector<int>& nums, int k) {
 }
 
 
+int median_of_two_sorted_arrays(vector<int> &a, vector<int> &b) {
+    int n1 = a.size();
+    int n2 = b.size();
+    if(n1 > n2) return median_of_two_sorted_arrays(b, a);
+    int low = 0, high = n1;
+    int left = (n1 + n2 + 1) / 2;         // total number of elements that should be present on left side of the partition
+    int n = n1 + n2;
 
+    while(low <= high) {
+        int mid1 = (low + high) >> 1;
+        int mid2 = left - mid1;
+        int l1 = INT_MIN, l2 = INT_MIN;
+        int r1 = INT_MAX, r2 = INT_MAX;
+        if(mid1 < n1) r1 = a[mid1];
+        if(mid2 < n2) r2 = b[mid2];
+        if(mid1 - 1 >= 0) l1 = a[mid1 - 1];
+        if(mid2 - 1 >= 0) l2 = b[mid2 - 1];
+
+        if(l1 <= r2 && l2 <= r1) {
+            if(n % 2 == 1) return max(l1, l2);
+            return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+        }
+        else if(l1 > r2) high = mid1 - 1;
+        else low = mid1 + 1;
+    }
+    return 0;
+}
 
 
 
