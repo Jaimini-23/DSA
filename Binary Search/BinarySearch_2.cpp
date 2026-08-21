020ba0c8cba0f9d81d015f508be1bd49abe1f7cb
Jaimini-23 <mayankjaimini23@gmail.com>
2026-08-21 20:27:17 +0530

    Find Peak Grid

--- Binary Search/BinarySearch_2.cpp
+++ Binary Search/BinarySearch_2.cpp
@@ -254,7 +254,39 @@ bool searchElement(vector<vector<int>> &arr, int k) {      // when each row and 
 }
 
 
-
+int findMaxIndex(vector<vector<int>> &arr, int n, int m, int col) {
+    int maxValue = -1;
+    int index = -1;
+    for(int i=0; i<n; i++) {
+        if(arr[i][col] > maxValue) {
+            maxValue = arr[i][col];
+            index = i;
+        }
+    }
+    return index;
+}
+vector<int> findPeakGrid(vector<vector<int>> &arr)  {            // peak element in 2D matrix
+    // TC: O(n logm) and SC: O(1)
+    int n = arr.size();
+    int m = arr[0].size();
+    int low = 0, high = m - 1;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        int maxRowIndex = findMaxIndex(arr, n, m, mid);
+        int left = mid - 1 >= 0 ? arr[maxRowIndex][mid - 1] : -1;
+        int right = mid + 1 < m ? arr[maxRowIndex][mid + 1] : -1;
+        if(arr[maxRowIndex][mid] > left && arr[maxRowIndex][mid] > right) {
+            return {maxRowIndex, mid};
+        }
+        else if(arr[maxRowIndex][mid] < left) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return {-1,-1};
+}
 
 
 
