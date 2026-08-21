21e48a304ec8467072fb7c6033dfc7bcc06ffa4e
Jaimini-23 <mayankjaimini23@gmail.com>
2026-08-21 20:48:47 +0530

    Median of 2D array

--- Binary Search/BinarySearch_2.cpp
+++ Binary Search/BinarySearch_2.cpp
@@ -289,6 +289,39 @@ vector<int> findPeakGrid(vector<vector<int>> &arr)  {            // peak element
 }
 
 
+int countSmallerEqual(vector<vector<int>> &matrix, int n, int m, int x) {
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+        cnt += upper_bound(matrix[i].begin(), matrix[i].end(), x) - matrix[i].begin();
+    }
+    return cnt;
+}
+int median_in_2D_array(vector<vector<int>> &matrix, int m, int n) {         // row wise sorted 2D array
+    int low = INT_MAX;
+    int high = INT_MIN;
+
+    n = matrix.size();
+    m = matrix[0].size();
+
+    for(int i = 0; i < n; i++) {
+        low = min(low, matrix[i][0]);
+        high = max(high, matrix[i][m - 1]);
+    }
+
+    int req = (n * m) / 2;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        int smallEqual = countSmallerEqual(matrix, n, m, mid);
+        if(smallEqual <= req)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return low;
+}
+
+
+
 
 int main() {
     int n ,r;
