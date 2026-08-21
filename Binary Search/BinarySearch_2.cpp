20dbadc4ead2a2ebd2a4d32521a3d071ccf19f91
Jaimini-23 <mayankjaimini23@gmail.com>
2026-08-21 10:49:36 +0530

    Row with max ones

--- Binary Search/BinarySearch_2.cpp
+++ Binary Search/BinarySearch_2.cpp
@@ -195,6 +195,44 @@ int Kth_element_of_two_sorted_arrays(vector<int> &a, vector<int> &b, int k) {
     return 0;
 }
 
+
+int lowerBound(vector<int> &arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        if(arr[mid] >= x) {
+            ans = mid;
+            high = mid - 1;
+        }
+        else low = mid + 1;
+    }
+    return ans;
+}
+int row_with_max_1(vector<vector<int>> &arr, int n, int m) {
+    int cntMax = 0;
+    int index = -1;
+    for(int i=0; i<n; i++) {
+        int cntOnes = m - lowerBound(arr[i], m, 1);
+        if(cntOnes > cntMax) {
+            cntMax = cntOnes;
+            index = i;
+        } 
+    }
+    return index;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 int main() {
     int n ,r;
     cin >> n >> r;
