e3a753de0d1e566378e9b804de843efb1f006759
Jaimini-23 <mayankjaimini23@gmail.com>
2026-09-03 19:45:28 +0530

    Delete Middle

--- LinkedList/LinkedList_1.cpp
+++ LinkedList/LinkedList_1.cpp
@@ -600,6 +600,40 @@ int lengthOfLoop(Node* head) {
 }
 
 
+Node* deleteMiddle(Node* head) {
+    // TC: O(n + n/2) and SC: O(1)
+    // if(head == NULL || head->next == NULL) return NULL;
+    // int len = lengthOfLL(head);
+    // int res = len / 2;
+    // Node* temp = head;
+    // while(temp != NULL) {
+    //     res--;
+    //     if(res == 0) {
+    //         Node* middle = temp->next;
+    //         temp->next = temp->next->next;
+    //         delete middle;
+    //         break;
+    //     }
+    //     temp = temp->next;
+    // }
+    // return head;
+
+    // TC: O(n/2) and SC: O(1)
+    if(head == NULL || head->next == NULL) return NULL;
+    Node* slow = head;
+    Node* fast = head;
+    fast = fast->next->next;
+    while(fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    Node* middle = slow->next;
+    slow->next = slow->next->next;
+    delete middle;
+    return head;
+}
+
+
 
 
 int main() {
