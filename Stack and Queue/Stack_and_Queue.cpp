57f032ae4089f6c4a8c9bd8eff947ec4fb58a69c
Jaimini-23 <mayankjaimini23@gmail.com>
2026-09-07 21:57:29 +0530

    Infix to Postfix

--- Stack and Queue/Stack_and_Queue.cpp
+++ Stack and Queue/Stack_and_Queue.cpp
@@ -1,6 +1,7 @@
 #include <bits\stdc++.h>
 using namespace std;
 
+
 bool validParenthesis(string str) {
     // TC: O(n) and SC: O(n)
     stack<char> st;
@@ -22,4 +23,47 @@ bool validParenthesis(string str) {
 }
 
 
+int priority(char ch) {
+    if(ch == '^') return 3;
+    else if(ch == '*' || ch == '/') return 2;
+    else if(ch == '+' || ch == '-') return 1;
+    return -1;
+}
+string infixToPostfix(string s) {
+    // TC: O(n) and SC: O(n)
+    stack<char> st;
+    string ans = "";
+    for(int i=0; i<s.size(); i++) {
+        if((s[i] >= 'a' && s[i] <= 'z') ||
+            (s[i] >= 'A' && s[i] <= 'Z') ||
+            (s[i] >= '0' && s[i] <= '9')) {
+                ans += s[i];
+        }
 
+        else if(s[i] == '(') st.push(s[i]);
+        else if(s[i] == ')') {
+            while(!st.empty() && st.top() != '(') {
+                ans += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+
+        else {
+            while(!st.empty() && st.top() != '(' &&
+                (priority(s[i]) < priority(st.top()) ||
+                (priority(s[i]) == priority(st.top()) && s[i] != '^'))) {
+    
+                ans += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+
+    while(!st.empty()) {
+        ans += st.top();
+        st.pop();
+    }
+    return ans;
+}
